83ce9d17a2ad27fb02aa9c9c768aa3b69172926f
robinsk8a <robinsk8a@gmail.com>
2023-10-25 21:17:21 -0500

    LocalStorage, date setup, little changes to style

--- script.js
+++ script.js
@@ -5,8 +5,17 @@ const questInput = document.querySelector('.add-quest__input');
 const questChecked = 'fa-check-circle';
 const questUnchecked = 'fa-circle';
 const lineChecked = 'line-through';
-let questId = 1;
+let questId;
+let QLIST;
 
+
+
+// date function
+const dateDisplay = new Date()
+date.innerHTML = dateDisplay.toLocaleDateString('en-US',{weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'})
+
+
+// ADD NEW ITEM
 const addNewQuest = function(quest, questId, done, del) {
 
     if (del) {
@@ -17,7 +26,7 @@ const addNewQuest = function(quest, questId, done, del) {
     const LINE = done? lineChecked : '';
 
     const liTemplate = `
-    <li class="quest-item id${questId}">
+    <li class="quest-item" id="${questId}">
                     <i class="fa-regular ${DONE} quest-item__check quest-item__icon" data="done"></i>
                     <p class="text ${LINE}">${quest}</p>
                     <i class="fa-solid fa-circle-xmark quest-item__cancel quest-item__icon" data="del"></i>
@@ -30,7 +39,17 @@ const newQuest = () => {
     const quest = questInput.value;
     if (quest) {
         addNewQuest(quest, questId, false, false);
-    };
+        // MAKING THE ITEM'S ARRAY
+        QLIST.push({
+            name: quest,
+            id: questId,
+            done: false,
+            del: false
+        })
+    // SETING THE LOCAL STORAGE
+    
+};
+    localStorage.setItem('TODO', JSON.stringify(QLIST))
     questInput.value = '';
     questInput.focus();
     questId++
@@ -41,11 +60,13 @@ const questDone = (element) => {
     element.classList.toggle(questChecked);
     element.classList.toggle(questUnchecked);
     element.parentNode.querySelector('.text').classList.toggle(lineChecked);
+    QLIST[element.parentNode.id].done = QLIST[element.parentNode.id].done ? false : true;
 };
 
 // DELETE LI ITEM (QUEST ITEM)
 const questDel = (element) => {
     element.parentNode.parentNode.removeChild(element.parentNode);
+    QLIST[element.parentNode.id].del = QLIST[element.parentNode.id].del ? false : true;
 };
 
 
@@ -64,4 +85,20 @@ questList.addEventListener('click', function(event) {
     } else if (element) {
         questDel(element)
     };
+    localStorage.setItem('TODO', JSON.stringify(QLIST))
 })
+
+
+let data = localStorage.getItem('TODO')
+if (data) {
+    QLIST = JSON.parse(data);
+    questId = QLIST.length;
+    (function() {
+        QLIST.forEach(function(i) {
+            addNewQuest(i.name, i.id, i.done, i.del);
+            })
+        })();  
+} else {
+    QLIST = [];
+    questId = 0
+}
